4125dd881673f8e32555e15436d15e39c1728595
R1Doonan <s2240879@ed.ac.uk>
2025-03-12 12:06:45 +0000

    fuckin cleanliness

--- practice.R
+++ practice.R
@@ -1,27 +1,73 @@
 # RMD
 # Diss data analysis
+ rm(list=ls())
 # load packages
 library(tidyverse)
 library(glmmTMB)
 library(MuMIn)
-#read in csv
-yst_data <- read_excel("data/RMD_sticky_trap_data.xlsx")
-view(yst_data)
+#read in csv's
+wasp_data <- read_excel("data/RMD_sticky_trap_data.xlsx")
+aphid_data <- read_excel("data/aphid_data.xlsx")
 
-str(yst_data)
+# explore data
+view(wasp_data)
+view(aphid_data)
+str(wasp_data)
+str(aphid_data)
 
-yst_data <- yst_data %>% 
-  mutate(across(c(sample_id, set_no, n_treatment, crop, margin, con_rel), as.factor))
+# cleaning (mutate)
+wasp_data <- wasp_data %>% 
+  mutate(across(c(set_no, n_treatment, crop, margin, con_rel), as.factor))
+aphid_data <- aphid_data %>%
+  mutate(across(c(set_no,sample_point), as.factor))
 
-mean(yst_data$aphidius_colemani)   #  mean
-var(yst_data$aphidius_colemani) # variance
+mean(wasp_data$aphidius_colemani)   #  mean
+var(wasp_data$aphidius_colemani) # variance
 
-# trialling differnet models and assessing AIC
-nb_m1 <- glmmTMB(aphidius_colemani~con_rel * margin + (1 | sample_id) + (1 | set_no),
-  family = nbinom2(),  # Variance = μ + μ²/θ
-  data = yst_data)
 
-nb_m1 <- glmmTMB(aphidius_colemani~con_rel * margin + (1 | sample_id) + (1 | set_no),
-                   family = nbinom2(),  # Variance = μ + μ²/θ
-                   data = yst_data)
-AICc(nb_m1)
+
+# average aphid and wasp data across time ----
+
+aphid_avg <- aphid_data %>% 
+      group_by(plot_id) %>% 
+  summarise(mean_aphid = mean(no_infested))
+
+wasp_avg <- wasp_data %>% 
+      group_by(plot_id) %>%
+  summarise(
+    mean_ervi = mean(aphidius_ervi),
+    mean_colemani = mean(aphidius_colemani),
+    mean_ichneumonid = mean(ichneumonid_spp.),
+    mean_other = mean(other_parasitica),
+    .groups = "drop"
+  )
+
+aphid_aggregated <- aphid_data %>%
+  group_by(plot_id) %>%
+  summarise(
+    total_infested = sum(no_infested),  # Total across all time/sample points
+    mean_infested = mean(no_infested),  # Mean per sample point
+    .groups = "drop"
+  )
+
+wasp_aggregated <- wasp_data %>%
+  group_by(plot_id) %>%
+  summarise(
+    mean_ervi = mean(aphidius_ervi),
+    mean_colemani = mean(aphidius_colemani),
+    mean_ichneumonid = mean(ichneumonid_spp.),
+    mean_other = mean(other_parasitica),
+    .groups = "drop"
+  ) %>%
+  # Convert plot_id "P1" to numeric plot_id 1
+  mutate(plot_id = as.numeric(gsub("P", "", plot_id))) %>%
+  arrange(plot_id) %>%
+  #select(-plot_id)  # Remove original ID column
+merged_data <- inner_join(
+  aphid_aggregated, 
+  wasp_aggregated, 
+  by = "plot_id"
+)
+view(wasp_aggregated)
+str(wasp_aggregated)
+
